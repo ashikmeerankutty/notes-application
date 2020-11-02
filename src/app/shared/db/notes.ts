6339209022c478311f46343b04034ed039caea49
Ashik Meerankutty <ashik9591@gmail.com>
2020-11-02 22:28:33 +0530

    Fixes in notes db

--- src/app/shared/db/notes.ts
+++ src/app/shared/db/notes.ts
@@ -1,5 +1,6 @@
 import { getItemFromStorage, setItemToStorage } from './storage';
 import { Note } from './types';
+import { v4 as uuid4 } from 'uuid';
 
 export const getNotes = () => {
   return getItemFromStorage('notes') || [];
@@ -10,13 +11,29 @@ export const addNote = (note: Note) => {
   setItemToStorage('notes', [...notes, note]);
 };
 
-export const deleteNote = (noteId: number) => {
+export const createNewNote = (title: string, notes: string): Note => {
+  const newNote: Note = {
+    id: uuid4(),
+    title,
+    notes,
+    archived: false,
+    pinned: false,
+    created: new Date().toISOString(),
+    lastUpdated: new Date().toISOString(),
+  };
+
+  addNote(newNote);
+
+  return newNote;
+};
+
+export const deleteNote = (noteId: string) => {
   const notes = getNotes();
   const filteredNotes = notes.filter((note: Note) => note.id !== noteId);
   setItemToStorage('notes', filteredNotes);
 };
 
-export const updateNote = (noteId: number, updatedNote: Note) => {
+export const updateNote = (noteId: string, updatedNote: Note) => {
   const notes = getNotes();
   const updatedNotes = notes.map((note: Note) =>
     note.id !== noteId ? note : updatedNote
@@ -24,7 +41,7 @@ export const updateNote = (noteId: number, updatedNote: Note) => {
   setItemToStorage('notes', updatedNotes);
 };
 
-export const getNote = (noteId: number) => {
+export const getNote = (noteId: string) => {
   const notes = getNotes();
   return notes.find((note: Note) => note.id === noteId);
 };
