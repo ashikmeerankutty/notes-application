48a8e9c7dc7675565d3221d052d3173fc780d3ff
Ashik Meerankutty <ashik9591@gmail.com>
2020-11-03 17:51:11 +0530

    Adding new note and list note to home page

--- src/app/pages/home/index.tsx
+++ src/app/pages/home/index.tsx
@@ -1,14 +1,22 @@
 /**@jsx jsx */
-import { jsx } from '@emotion/core';
+import { css, jsx } from '@emotion/core';
 import React from 'react';
-import { Button } from 'components';
+import ListNotes from './listNotes';
+import NewNote from './newNotes';
+
+const homeStyles = css`
+  padding: 30px;
+  display: flex;
+  flex-direction: column;
+`;
 
 interface HomeProps {}
 
 const Home: React.FC<HomeProps> = () => {
   return (
-    <div>
-      <Button />
+    <div css={homeStyles}>
+      <NewNote key="new" />
+      <ListNotes key="list" />
     </div>
   );
 };
