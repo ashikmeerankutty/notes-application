f3615939fe3edf532754d6b53b50da95d1f916c2
Ashik Meerankutty <ashik9591@gmail.com>
2020-11-04 00:37:50 +0530

    Adding modal component

---
+++ src/app/shared/components/modal/index.tsx
@@ -0,0 +1,139 @@
+/**@jsx jsx */
+import { css, jsx } from '@emotion/core';
+import { FunctionComponent, useEffect, useCallback, useRef } from 'react';
+import { colors, Portal } from 'components';
+import { IconButton } from '../iconButton';
+import { CrossIcon } from '@space-kit/icons';
+
+const wrapperStyles = css`
+  position: fixed;
+  z-index: 999;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  display: flex;
+  background-color: rgba(67, 90, 111, 0.7);
+  justify-content: center;
+  align-items: flex-start;
+`;
+
+const modalStyles = (width: number = 560) => css`
+  display: flex;
+  flex-direction: column;
+  background-color: ${colors.white};
+  margin-top: 150px;
+  width: ${width}px;
+  border-radius: 5px;
+  max-height: calc(100% - 300px);
+`;
+
+const contentStyles = (padding: number = 20) => css`
+  padding: ${padding}px;
+  overflow: auto;
+`;
+
+const headerStyles = () => css`
+  padding: 16px;
+  display: flex;
+  flex-shrink: 0;
+  justify-content: space-between;
+  .headerStyles__text {
+    font-size: 20px;
+    font-weight: 500;
+  }
+  border-bottom: 1px solid ${colors.neutral4};
+`;
+
+const footerStyles = () => css`
+  padding: 16px;
+  display: flex;
+  flex-shrink: 0;
+  justify-content: flex-end;
+  border-top: 1px solid ${colors.neutral4};
+`;
+
+interface ModalProps {
+  show: boolean;
+  children: React.ReactNode;
+  width?: number;
+  padding?: number;
+  onClose: () => void;
+  header?: boolean;
+  headerText?: string;
+}
+
+export const Modal: FunctionComponent<ModalProps> = ({
+  show,
+  children,
+  width,
+  padding,
+  onClose,
+  header,
+  headerText,
+}: ModalProps) => {
+  const targetRef = useRef(null);
+
+  const handleBodyClick = useCallback(
+    (event) => {
+      if (targetRef.current && targetRef.current.contains(event.target)) {
+        return;
+      }
+      onClose();
+    },
+    [targetRef.current, onClose]
+  );
+
+  const handleOnEsc = useCallback(
+    (event) => {
+      if (event.key === 'Escape') {
+        onClose();
+      }
+    },
+    [onClose]
+  );
+
+  useEffect(() => {
+    if (show) {
+      document.body.addEventListener('click', handleBodyClick, false);
+      document.body.addEventListener('keydown', handleOnEsc, false);
+    } else {
+      document.body.removeEventListener('click', handleBodyClick, false);
+      document.body.removeEventListener('keydown', handleOnEsc, false);
+    }
+    return () => {
+      document.body.removeEventListener('click', handleBodyClick, false);
+      document.body.removeEventListener('keydown', handleOnEsc, false);
+    };
+  }, [show]);
+
+  return (
+    <div>
+      {show && (
+        <Portal>
+          <div css={wrapperStyles}>
+            <div ref={targetRef} css={modalStyles(width)}>
+              {header && (
+                <div css={headerStyles}>
+                  <p className="headerStyles__text">{headerText}</p>
+                  <IconButton
+                    onClick={() => onClose()}
+                    icon={<CrossIcon size={24} />}
+                  />
+                </div>
+              )}
+              <div css={contentStyles(padding)}>
+                <div>{children}</div>
+              </div>
+              {header && (
+                <div css={footerStyles}>
+                  <button>Submit</button>
+                </div>
+              )}
+            </div>
+          </div>
+        </Portal>
+      )}
+    </div>
+  );
+};
