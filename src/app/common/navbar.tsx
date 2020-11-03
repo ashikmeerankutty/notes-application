5bb8d104b577800513e72c08b3a9c534e6e9f7b0
Ashik Meerankutty <ashik9591@gmail.com>
2020-11-03 17:49:03 +0530

    Adding navbar

---
+++ src/app/common/navbar.tsx
@@ -0,0 +1,35 @@
+/**@jsx jsx */
+import { css, jsx } from '@emotion/core';
+import React from 'react';
+import { colors } from '../shared/components/themes';
+
+interface NavbarProps {
+  setTheme: () => void;
+}
+
+const navbarStyles = css`
+  position: fixed;
+  top: 0;
+  width: 100%;
+  height: 64px;
+  border-bottom: 1px solid ${colors.neutral5};
+  box-sizing: border-box;
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const Navbar: React.FC<NavbarProps> = ({ setTheme }: NavbarProps) => {
+  return (
+    <header role="banner" css={navbarStyles}>
+      <button type="button">Hamburger</button>
+      <button
+        type="button"
+        onClick={() => {
+          setTheme();
+        }}
+      >
+        Toggle
+      </button>
+    </header>
+  );
+};
