046e8c293b9c6bab9cfd81954dce93b988ca39df
Ashik Meerankutty <ashik9591@gmail.com>
2020-11-03 17:51:23 +0530

    Adding notes to reducer

--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -1,10 +1,13 @@
 import { combineReducers } from 'redux';
 import globals, { GlobalStates } from './globals';
+import notes, { NoteState } from './notes';
 
 export interface State {
   globals: GlobalStates;
+  notes: NoteState;
 }
 
 export default combineReducers({
   globals,
+  notes,
 });
