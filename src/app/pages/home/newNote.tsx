a350f1890726afbebfb64a80402b8a69ca407526
Ashik Meerankutty <ashik9591@gmail.com>
2020-11-05 08:27:52 +0530

    Updateing styles of new note modal

--- src/app/pages/home/newNote.tsx
+++ src/app/pages/home/newNote.tsx
@@ -1,12 +1,11 @@
 /**@jsx jsx */
 import { css, jsx } from '@emotion/core';
-import { EyeOpenIcon, PinIcon } from '@space-kit/icons';
-import { IconButton, Modal, Theme } from 'components';
+import { Theme } from 'components';
 import { useTheme } from 'emotion-theming';
-import React, { ChangeEvent, useEffect, useRef, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
-import { loadNotes, createNewNote } from '../../actions/notes';
-import Markdown from './markdown';
+import { loadNotes } from '../../actions/notes';
+import NoteModal, { noteInputStyle } from './noteModal';
 
 const newNoteStyles = (theme: Theme) => css`
   display: flex;
@@ -20,88 +19,25 @@ const newNoteStyles = (theme: Theme) => css`
   cursor: text;
 `;
 
-const noteTitleStyles = css`
-  display: flex;
-`;
-
-const noteInputStyle = (theme: Theme) => css`
-  width: 100%;
-  border: none;
-  outline: none;
-  font-size: 15px;
-  font-weight: 500;
-  background: ${theme.colors.background};
-  color: ${theme.colors.text};
-`;
-
-const noteDescriptionStyles = (theme: Theme) => css`
-  width: 100%;
-  border: none;
-  outline: none;
-  font-size: 14px;
-  background: ${theme.colors.background};
-  color: ${theme.colors.text};
-  flex-grow: 1;
-  flex-basis: 0;
-`;
-
-const notesToolbarStyles = css`
-  display: flex;
-  justify-content: space-between;
-  margin-top: 20px;
-`;
-
-const noteViewStyles = css`
-  margin-top: 20px;
-  display: flex;
-`;
-
-const notePreviewStyles = css`
-  margin-top: 0;
-  flex-grow: 1;
-  flex-basis: 0;
-`;
-
 interface NewNoteProps {}
 
 const NewNote: React.FC<NewNoteProps> = () => {
-  const [title, setTitle] = useState('');
-  const [note, setNote] = useState('');
-  const [preview, setPreview] = useState(false);
   const [showModal, setShowModal] = useState(false);
-  const textAreaRef = useRef<HTMLTextAreaElement>(null);
 
   const theme = useTheme<Theme>();
 
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(loadNotes('', 1, 10));
+    dispatch(loadNotes('', 1, 100));
   }, [dispatch]);
-
-  const saveNote = () => {
-    if (title || note) {
-      dispatch(createNewNote(title, note));
-    }
-  };
-
-  const onPreviewClick = () => {
-    setPreview(!preview);
-  };
-
-  const showNewNote = () => {
-    setShowModal(true);
-    if (textAreaRef && textAreaRef.current) {
-      console.log(textAreaRef.current);
-    }
-  };
 
   return (
     <div>
       <div
         css={newNoteStyles}
         onClick={() => {
-          showNewNote();
+          setShowModal(true);
         }}
       >
         <input
@@ -113,51 +49,7 @@ const NewNote: React.FC<NewNoteProps> = () => {
           placeholder="Take a note.."
         />
       </div>
-      <Modal onClose={() => setShowModal(false)} show={showModal}>
-        <div css={noteTitleStyles}>
-          <input
-            css={noteInputStyle(theme)}
-            aria-label="note title"
-            key="title"
-            value={title}
-            onChange={(e: ChangeEvent<HTMLInputElement>) => setTitle(e.target.value)}
-            type="text"
-            placeholder="Title"
-          />
-          <IconButton Icon={PinIcon} />
-        </div>
-        <div css={noteViewStyles}>
-          <textarea
-            css={noteDescriptionStyles(theme)}
-            rows={10}
-            aria-label="note"
-            placeholder="Take a note.."
-            key="note"
-            value={note}
-            onChange={(e: ChangeEvent<HTMLTextAreaElement>) =>
-              setNote(e.target.value)
-            }
-            ref={textAreaRef}
-          />
-          {preview && (
-            <div css={notePreviewStyles}>
-              <Markdown text={note} />
-            </div>
-          )}
-        </div>
-        <div css={notesToolbarStyles}>
-          <div>
-            <IconButton
-              active={preview}
-              onClick={onPreviewClick}
-              Icon={EyeOpenIcon}
-            />
-          </div>
-          <button key="add" onClick={saveNote} type="button">
-            Save Note
-          </button>
-        </div>
-      </Modal>
+      {showModal && <NoteModal onClose={() => setShowModal(false)} />}
     </div>
   );
 };
